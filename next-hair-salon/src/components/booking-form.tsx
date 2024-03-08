4f780b4f762567ed2d59aadce5cb6491cc8d091e
LamaPirat <g.skrefsrud@gmail.com>
2024-03-08 15:38:32 +0100

    add: enums for BookingType and AnimationDirection, States for bookingData and uiState, Access to the states, generic function for updating bookingData values, BookingStep component for determening what component shows and from what direction it shall animate from

--- next-hair-salon/src/components/booking-form.tsx
+++ next-hair-salon/src/components/booking-form.tsx
@@ -1,25 +1,94 @@
 "use client";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Select } from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
-import { Checkbox } from "@/components/ui/checkbox";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+
 import CalendarSelect from "./bookingComponents/CalendarSelect";
 import SlideInContainer from "./SlideInContainer";
+import React, { useState } from "react";
+import Stats from "./Stats";
 
 export function BookingForm() {
+  enum BookingType {
+    HERREKLIPP = "herreklipp",
+    DAMEKLIPP = "dameklipp",
+    HÅRFARGING = "hårfarging",
+    SKJEGGTRIMM = "skjeggtrim",
+  }
+
+  enum AnimationDirection {
+    TOP = "top",
+    BOTTOM = "bottom",
+  }
+
+  //BookingData state
+  const [bookingData, setBookingData] = useState({
+    employee: {
+      id: Number,
+      employee_name: String,
+    },
+    bookingDetails: {
+      date: Date,
+      time: String,
+      bookingType: [BookingType],
+      bookingDuration: Number,
+    },
+    customer: {
+      customer_name: String,
+      phoneNumber: Number,
+      comment: String,
+    },
+  });
+
+  //Access the bookingData objects
+  const { employee, bookingDetails, customer } = bookingData;
+
+  //Generic function to update bookingData
+  function updateBookingData(keyPath, value) {
+    setBookingData((prevBookingData) => ({
+      ...prevBookingData,
+      ...keyPath.reduceRight((acc, key) => ({ [key]: acc }), value),
+    }));
+  }
+
+  //UI Component State
+  const [uiState, setUiState] = useState({
+    selectedOption: 0,
+    previousOption: null,
+    direction: AnimationDirection.BOTTOM,
+  });
+
+  //Access the states
+  const { selectedOption, previousOption, direction } = uiState;
+
   const handleDateSelection = (selectedDate: Date, displayDate: String) => {
-    console.log("Selected date:", selectedDate);
-    console.log("Display date: ", displayDate);
+    updateBookingData(["bookingDetails", "date"], selectedDate);
+  };
+
+  //Handling what component should be rendered based on the selectedOption variable
+  const BookingStep = ({ selectedOption, stepIndex, direction, children }) => {
+    return (
+      selectedOption === stepIndex && (
+        <SlideInContainer direction={direction}>{children}</SlideInContainer>
+      )
+    );
   };
 
   return (
     <div className='bg-white w-1/2 h-2/3'>
-      <SlideInContainer>
+      <BookingStep
+        selectedOption={selectedOption}
+        stepIndex={0}
+        direction={direction}
+      >
         <CalendarSelect onSelect={handleDateSelection} />
-      </SlideInContainer>
+      </BookingStep>
+
+      <BookingStep
+        selectedOption={selectedOption}
+        stepIndex={1}
+        direction={direction}
+      >
+        <Stats />
+      </BookingStep>
+      <p>{bookingDetails.date.toLocaleString()}</p>
     </div>
   );
 }
